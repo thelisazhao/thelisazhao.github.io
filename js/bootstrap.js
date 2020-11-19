c51d943af4ac90ff7da92f77bd3e5816e1a2339f
yuqing.zhao@ucalgary.ca <Lisa Zhao>
2020-11-19 16:05:12 -0700

    Testing Recaptcha

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,6 +3,37 @@
   * Copyright 2011-2020 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
+//function validateForm() {
+//  var name =  document.getElementById('name').value;
+//  if (name == "") {
+//      document.querySelector('.status').innerHTML = "Name cannot be empty";
+//      return false;
+//  }
+//  var email =  document.getElementById('email').value;
+//  if (email == "") {
+//      document.querySelector('.status').innerHTML = "Email cannot be empty";
+//      return false;
+//  } else {
+//      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//      if(!re.test(email)){
+//          document.querySelector('.status').innerHTML = "Email format invalid";
+//          return false;
+//      }
+//  }
+//  var subject =  document.getElementById('subject').value;
+//  if (subject == "") {
+//      document.querySelector('.status').innerHTML = "Subject cannot be empty";
+//      return false;
+//  }
+//  var message =  document.getElementById('message').value;
+//  if (message == "") {
+//      document.querySelector('.status').innerHTML = "Message cannot be empty";
+//      return false;
+//  }
+//  document.querySelector('.status').innerHTML = "Sending...";
+//}
+
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery'), require('popper.js')) :
   typeof define === 'function' && define.amd ? define(['exports', 'jquery', 'popper.js'], factory) :
@@ -4415,4 +4446,6 @@
   Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
+
+
 //# sourceMappingURL=bootstrap.js.map
